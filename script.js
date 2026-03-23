fc55749004719016a22256d94363d347645b83bf
FADY SHERIF <fadysherif066@gmail.com>
2026-03-23 19:34:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
-
-let grades = [80, 65, 90, 70, 50];
+let grades = [];
+let count = prompt("Enter how many grades you want to input:");
+count = parseInt(count);
+for (let i = 0; i < count; i++) {
+  let grade = prompt("Enter grade " + (i + 1) + ":");
+  grades.push(parseInt(grade));    
+}
 let sum = 0;
 for (let i = 0; i < grades.length; i++) {
   sum += grades[i];
 }
+
+
 let average = sum / grades.length;
 console.log("The average is: " + average);
 
-if (average >= 85) {
+
+if (average >= 8.5) {
   console.log("Excellent 👏");
-} else if (average >= 70) {
+} else if (average >= 7) {
   console.log("Very Good 👍");
-} else if (average >= 50) {
-  console.log("Pass 🙂");
-} else {
+} else if (average >= 6) {
+  console.log("pass 🙂");
+}  else {
   console.log("Fail ❌");
 }
